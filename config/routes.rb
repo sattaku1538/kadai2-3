8bf73f4b994534ac773511d8f1de1d83355c8310
sattaku1538 <choose.chose.chosen@gmail.com>
2021-11-07 01:30:56 +0000

    update but NO finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   root 'homes#top'
-  get 'home/about' => 'homes#about'
+  get "home/about" => "homes#about"
+
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index, :show, :edit, :update]
   
 end
